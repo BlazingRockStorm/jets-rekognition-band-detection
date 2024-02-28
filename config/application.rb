1b4d04e431efee323b458067ea1e8183f323cb31
BlazingRockStorm <rocker.gryphon@gmail.com>
2024-02-28 22:27:45 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,12 @@
+module JetsRekognitionBandDetection
+  class Application < Jets::Application
+    config.load_defaults 5.0
+
+    config.project_name = "jets-rekognition-band-detection"
+    config.mode = "html"
+
+    # Docs:
+    # https://rubyonjets.com/docs/config/
+    # https://rubyonjets.com/docs/config/reference/
+  end
+end
